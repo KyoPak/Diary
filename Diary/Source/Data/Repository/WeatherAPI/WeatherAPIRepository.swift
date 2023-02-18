a59f9b601ad172a39fc03f14dd465425992d0482
Kyo <hyosing92@gmail.com>
2023-02-19 01:17:13 +0900

    feat : 기존 NetworkManager 참고하여 WeatherAPIRepository 구현

--- Diary/Source/Data/Repository/WeatherAPI/WeatherAPIRepository.swift
+++ Diary/Source/Data/Repository/WeatherAPI/WeatherAPIRepository.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+final class WeatherAPIRepository {
+    func fetch(url: URL, completion: @escaping (Result<Data, SessionError>) -> Void) {
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "GET"
+        
+        let sesseion = URLSession(configuration: .default)
+        
+        sesseion.dataTask(with: urlRequest) { data, response, error in
+            guard error == nil else {
+                completion(.failure(.networkError))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse,
+                  (200..<300).contains(response.statusCode) else {
+                completion(.failure(.networkError))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.noneDataError))
+                return
+            }
+            
+            completion(.success(data))
+
+        }.resume()
+    }
+}
