ce7c6430c07b2c69cc790898daa3e8620e3b8483
Kyo <hyosing92@gmail.com>
2023-02-19 22:55:32 +0900

    feat : FetchDiaryReportsUseCase, DeleteDiaryReportUseCase 파일 생성

---
+++ Diary/Source/Domain/UseCase/DeleteDiaryReportUseCase.swift
@@ -0,0 +1,12 @@
+//
+//  DeleteDiaryReportUseCase.swift
+//  Diary
+//
+//  Created by Kyo on 2023/02/19.
+//
+
+import Foundation
+
+final class DeleteDiaryReportUseCase {
+    
+}
---
+++ Diary/Source/Domain/UseCase/FetchDiaryReportsUseCase.swift
@@ -0,0 +1,12 @@
+//
+//  FetchDiaryReportsUseCase.swift
+//  Diary
+//
+//  Created by Kyo on 2023/02/19.
+//
+
+import Foundation
+
+final class FetchDiaryReportsUseCase {
+    
+}
