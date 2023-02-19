27091e94694b9a1db8ab3ea532bcd0a22dedc321
Kyo <hyosing92@gmail.com>
2023-02-20 00:24:46 +0900

    feat : ListViewModel 기본 구현

--- Diary/Source/Presentation/Scene/ViewModel/ListViewModel.swift
+++ Diary/Source/Presentation/Scene/ViewModel/ListViewModel.swift
@@ -7,6 +7,54 @@
 
 import Foundation
 
-class ListViewModel {
+final class ListViewModel {
+    private let fetchUseCase: FetchDiaryReportsUseCase
+    private let deleteUseCase: DeleteDiaryReportUseCase
     
+    private var diaryReports: [DiaryReport] = [] {
+        didSet {
+            dataHandler?(diaryReports)
+        }
+    }
+    
+    private var dataHandler: (([DiaryReport]) -> Void)?
+    
+    init(fetchUseCase: FetchDiaryReportsUseCase, deleteUseCase: DeleteDiaryReportUseCase) {
+        self.fetchUseCase = fetchUseCase
+        self.deleteUseCase = deleteUseCase
+        fetchData()
+    }
+    
+    private func fetchData() {
+        let result = fetchUseCase.fetchData()
+        
+        switch result {
+        case .success(let datas):
+            diaryReports = datas
+        case .failure(let error):
+            sendError(error: error)
+        }
+    }
+    
+    private func sendError(error: Error) {
+        guard let error = error as? DataError else { return }
+        print(error)
+        // TODO: Delegate 호출하여 얼럿 팝업
+    }
+}
+
+extension ListViewModel {
+    func bindData(completion: @escaping ([DiaryReport]) -> Void) {
+        dataHandler = completion
+    }
+    
+    func deleteData(index: Int) {
+        let deleteData = diaryReports.remove(at: index)
+        
+        do {
+            try deleteUseCase.deleteData(id: deleteData.id)
+        } catch {
+            sendError(error: error)
+        }
+    }
 }
