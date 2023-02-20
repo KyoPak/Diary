fdb70da740ee9fba4d7d15319499cac63998d111
Kyo <hyosing92@gmail.com>
2023-02-20 23:24:06 +0900

    feat : DetailViewModel 구현

--- Diary/Source/Presentation/DetailScene/ViewModel/DetailViewModel.swift
+++ Diary/Source/Presentation/DetailScene/ViewModel/DetailViewModel.swift
@@ -2,11 +2,39 @@
 //  DetailViewModel.swift
 //  Diary
 //
-//  Created by parkhyo on 2023/02/20.
+//  Created by Kyo on 2023/02/20.
 //
 
 import Foundation
 
 final class DetailViewModel {
+    private let fetchWeatherDataUseCase: FetchWeatherDataUseCase
+    private var diary: DiaryReport
     
+    init(data: DiaryReport?, fetchWeatherDataUseCase: FetchWeatherDataUseCase) {
+        self.fetchWeatherDataUseCase = fetchWeatherDataUseCase
+        guard let data = data else {
+            diary = DiaryReport(
+                id: UUID(),
+                contentText: "",
+                createdAt: Date(),
+                weather: CurrentWeather()
+            )
+            
+            return
+        }
+        diary = data
+    }
+}
+
+extension DetailViewModel {
+    func bindData(completion: @escaping (DiaryReport) -> Void) {
+        completion(diary)
+    }
+    
+    func fetchWeatherData(lat: String, long: String) {
+        fetchWeatherDataUseCase.fetchWeatherData(lat: lat, lon: long) { data in
+            self.diary.weather = data
+        }
+    }
 }
