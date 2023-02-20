f367385683fbbe6a52a9fb6a3d27ae72c810a245
Kyo <hyosing92@gmail.com>
2023-02-20 20:45:05 +0900

    feat : DetailViewController, DetailViewModel 파일 생성

---
+++ Diary/Source/Presentation/DetailScene/View/DetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DetailViewController.swift
+//  Diary
+//
+//  Created by Kyo on 2023/02/20.
+//
+
+import UIKit
+
+final class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Diary/Source/Presentation/DetailScene/ViewModel/DetailViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  DetailViewModel.swift
+//  Diary
+//
+//  Created by parkhyo on 2023/02/20.
+//
+
+import Foundation
+
+final class DetailViewModel {
+    
+}
