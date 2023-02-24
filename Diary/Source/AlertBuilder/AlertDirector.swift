2e7ad6de2d1d003be28bd10c8f5b37367be03dc8
Kyo <hyosing92@gmail.com>
2023-02-24 15:56:29 +0900

    feat : AlertDirector 생성하여 해당 클래스에서 AlertBuilder set하는 기능 추가

---
+++ Diary/Source/AlertBuilder/AlertDirector.swift
@@ -0,0 +1,35 @@
+//
+//  AlertDirector.swift
+//  Diary
+//
+//  Created by Kyo on 2023/02/24.
+//
+
+import UIKit
+
+final class AlertDirector {
+    private let alert = ConcreteAlertBuilder()
+    func setupAlert(title: String, message: String) -> UIAlertController {
+        return alert
+            .setupTitle(title)
+            .setupMessage(message)
+            .setupAction(title: "취소", style: .cancel, handler: nil)
+            .build()
+    }
+    
+    func setupActionSheet(title: String, message: String) -> UIAlertController {
+        return alert
+            .setupStyle(.actionSheet)
+            .setupTitle(title)
+            .setupMessage(message)
+            .setupAction(title: "취소", style: .cancel, handler: nil)
+            .build()
+    }
+    
+    func addAction(title: String,
+                   style: UIAlertAction.Style,
+                   handler: ((UIAlertAction) -> Void)?
+    ) {
+        alert.setupAction(title: title, style: style, handler: handler)
+    }
+}
