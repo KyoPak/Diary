77f56c0245ba7771ed92b59ec8ffde41fe2aaf04
Kyo <hyosing92@gmail.com>
2023-02-24 18:53:18 +0900

    refactor : CellViewModel 내부 cacheUseCase 프로퍼티 추가 및 imageData fetch 시 캐시 관련 로직 추가

--- Diary/Source/Presentation/Scene/ViewModel/CellViewModel.swift
+++ Diary/Source/Presentation/Scene/ViewModel/CellViewModel.swift
@@ -11,9 +11,16 @@ final class CellViewModel {
     private let diary: DiaryReport
     private let weatherImageUseCase: LoadWeatherImageUseCase
     
-    init(diary: DiaryReport, weatherImageUseCase: LoadWeatherImageUseCase) {
+    private let cacheUseCase: CheckCacheUseCase
+    
+    init(
+        diary: DiaryReport,
+        weatherImageUseCase: LoadWeatherImageUseCase,
+        cacheUseCase: CheckCacheUseCase
+    ) {
         self.diary = diary
         self.weatherImageUseCase = weatherImageUseCase
+        self.cacheUseCase = cacheUseCase
     }
     
     func bindData(completion: @escaping (DiaryReport) -> Void) {
@@ -22,10 +29,16 @@ final class CellViewModel {
     
     func fetchImageData(completion: @escaping (Data) -> Void) {
         guard let iconID = diary.weather.iconID else { return }
-        weatherImageUseCase.loadImage(id: iconID) { data in
-            
-            DispatchQueue.main.async {
-                completion(data)
+        
+        if let data = cacheUseCase.check(id: iconID) {
+            completion(data)
+        } else {
+            weatherImageUseCase.loadImage(id: iconID) { data in
+                
+                DispatchQueue.main.async {
+                    completion(data)
+                    self.saveCacheData(id: iconID, data: data)
+                }
             }
         }
     }
